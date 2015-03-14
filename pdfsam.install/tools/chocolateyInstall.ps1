80036c7d22b3b8d15df27275e4836d2b3e9de6b9
C-Duv <github.accounts@claude.duvergier.fr>
2015-03-14 02:13:47 +0100

    Updated to use 2.2.4 installer.
    Upstream now offer only one installer (while v2.2.2 were available in
    32-bits AND 64-bits):
    
        There is only one installer now. If you want to run 64 bits you can
        install Java 64 bits and run PDFsam with it.
    
            - Andrea Vacondio

--- pdfsam.install/tools/chocolateyInstall.ps1
+++ pdfsam.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 $packageName = 'pdfsam.install'
 $installerType = 'msi'
-$url = 'http://downloads.sourceforge.net/project/pdfsam/pdfsam/2.2.2/pdfsam-x86-v2_2_2.msi'
-$url64 = 'http://downloads.sourceforge.net/project/pdfsam/pdfsam/2.2.2/pdfsam-x64-v2_2_2.msi'
+$url = 'https://github.com/torakiki/pdfsam-v2/releases/download/v2.2.4/pdfsam-v2_2_4.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
