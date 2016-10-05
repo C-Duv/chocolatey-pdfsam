413ef552b0ad70b8501fc5cb98a892fdd302aeb4
C-Duv <github.accounts@claude.duvergier.fr>
2016-10-05 18:29:49 +0200

    Adds checksums for downloaded remote files
    Chocolatey can perform checksum verifications on downloaded files (such
    as installers). They have announced that checksums would be mandatory
    (date not yet determined).
    
    This commit adds SHA256 checksum of PDFsam Basic MSI installer.
    
    Issue: #7

--- pdfsam.install/tools/chocolateyInstall.ps1
+++ pdfsam.install/tools/chocolateyInstall.ps1
@@ -4,6 +4,8 @@ $packageInstallArgs = @{
     url            = 'https://github.com/torakiki/pdfsam/releases/download/v3.0.2.RELEASE/pdfsam-v3.0.2.RELEASE.msi'
     silentArgs     = '/quiet CHECK_FOR_UPDATES=false SKIPTHANKSPAGE=Yes'
     validExitCodes = @(0)
+    checksum       = 'da1e043869ae15cf2e047793091a021672ae4a9d57cf951b8235e18efa42b194'
+    checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageInstallArgs
