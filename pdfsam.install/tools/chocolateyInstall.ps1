c37cbbac5409f704e860ddcd4258b3f33d116c54
C-Duv <github.accounts@claude.duvergier.fr>
2016-10-12 01:03:08 +0200

    Updates package to PDFsam Basic v3.2.1
    This commits makes the package install PDFsam Basic v3.2.1.
    
    See [PDFsam blog](http://www.pdfsam.org/pdf-split-and-merge/new-release/pdfsam-basic-3-2-1-released/1649/)
    for more information.

--- pdfsam.install/tools/chocolateyInstall.ps1
+++ pdfsam.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageInstallArgs = @{
     packageName    = 'pdfsam.install'
     fileType       = 'msi'
-    url            = 'https://github.com/torakiki/pdfsam/releases/download/v3.1.0.RELEASE/pdfsam-v3.1.0.msi'
+    url            = 'https://github.com/torakiki/pdfsam/releases/download/v3.2.1.RELEASE/pdfsam-v3.2.1.msi'
     silentArgs     = '/quiet CHECK_FOR_UPDATES=false SKIPTHANKSPAGE=Yes'
     validExitCodes = @(0)
-    checksum       = '44c954416e5b2e52c73655f5d531501d4e6ed02d3eaf0b9814cb0ee458ffa6c6'
+    checksum       = '69e0a26635e47fc5fcb2820d5eb697285bc7644021fc34d0c9b26906142095b7'
     checksumType   = 'sha256'
 }
 
